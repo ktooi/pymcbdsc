3234455848d8f307aa1cf9f11277b19293855319
Kodai Tooi <26849015+ktooi@users.noreply.github.com>
2021-01-12 16:15:07 +0900

    Support for the Python3.5, 3.6 and 3.7.

--- tests/pymcbdsc/__init__.py
+++ tests/pymcbdsc/__init__.py
@@ -3,6 +3,43 @@ from unittest import mock
 import pymcbdsc
 import re
 import os
+from sys import version_info
+from logging import getLogger
+
+
+logger = getLogger(__name__)
+
+
+def stop_patcher(patcher, patcher_name=None):
+    """ mock.patch を停止する関数。
+
+    Python3.5, 3.6, 3.7 では、既に `stop()` をコールした patch で再度 `stop()` をコールすると
+    次のような Exception が Raise されてしまうので、その対処を行う。
+
+    >>> patcher = mock.patch('hoge')
+    >>> patcher.start()
+    >>> patcher.stop()
+    >>> patcher.stop()  # すでに `stop()` をコールした patch で再度 `stop()` をコールする。
+    Traceback (most recent call last):
+    ...snip...
+    RuntimeError: stop called on unstarted patcher
+
+    一度 `stop()` をコールした patch からは "is_local" 属性がなくなるので、 "is_local" 属性の有無で
+    patch の `stop()` をコールするか否かを判断する。ハック的だがやり方として問題ないかは知らん。
+
+    なお、 Python3.8, 3.9 では `stop()` をコールした patch で再度 `stop()` しても問題はないので、
+    このハックは利用せずに、何も気にしないで `stop()` をコールする。
+    """
+    (major, minor) = version_info[0:2]
+    if major == 3 and (minor >= 5 and minor <= 7):  # Python3.5, 3.6, 3.7 のみハックを利用。
+        if hasattr(patcher, "is_local"):
+            patcher.stop()
+        else:
+            if patcher_name is None:
+                patcher_name = patcher.attribute
+            logger.info("Trying stop a patcher named {name} but it was already stopped.".format(name=patcher_name))
+    else:  # Python3.5, 3.6, 3.7 以外はハックを利用しない。
+        patcher.stop()
 
 
 class TestMcbdscDownloader(unittest.TestCase):
@@ -18,8 +55,8 @@ class TestMcbdscDownloader(unittest.TestCase):
         self.mcbdsc = pymcbdsc.McbdscDownloader()
 
     def tearDown(self) -> None:
-        self.patcher_docker.stop()
-        self.patcher_requests.stop()
+        stop_patcher(self.patcher_docker)
+        stop_patcher(self.patcher_requests)
 
     def _test_root_dir(self, os_name, exp):
         p = mock.patch('pymcbdsc.os_name', os_name)
