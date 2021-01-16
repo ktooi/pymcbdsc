cb008699dccdd0feff793a614fe26683a0884bf3
Kodai Tooi <26849015+ktooi@users.noreply.github.com>
2021-01-16 19:27:53 +0900

    Modified the code
    to specify the scope of responsibility for each class.

--- pymcbdsc/__init__.py
+++ pymcbdsc/__init__.py
@@ -1,8 +1,10 @@
+from typing import List
 import docker
 from docker.client import DockerClient
 import requests
 import re
 import os.path
+from os import listdir
 # To can mock the os.name like below line:
 # >>> os_name = "posix"
 # >>> p = mock.patch('pymcbdsc.os_name', os_name)
@@ -10,6 +12,9 @@ from os import name as os_name
 
 
 __version__ = "0.1.0"
+
+
+bds_zip_file_pat = "bedrock-server-([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)\\.zip"
 
 
 def pymcbdsc_root_dir():
@@ -43,6 +48,9 @@ class McbdscDownloader(object):
     このクラスを利用するにあたっては、本モジュールのライセンスの他に Minecraft End User License Agreement
     及び Privacy Policy に同意する必要があります。
 
+    このクラスが持つ責任範囲は、 BDS の最新版のバージョン番号を取得することと、
+    そのファイルをダウンロードしローカルに保存することです。
+
     This class is download and manage latest version of the Bedrock Server file.
 
     You have to agree to the Minecraft End User License Agreement and Privacy Policy to use this module.
@@ -64,8 +72,7 @@ class McbdscDownloader(object):
     def __init__(self,
                  pymcbdsc_root_dir: str = pymcbdsc_root_dir(),
                  url: str = "https://www.minecraft.net/en-us/download/server/bedrock/",
-                 zip_url_pat: str = ("https:\\/\\/minecraft\\.azureedge\\.net\\/bin-linux\\/"
-                                     "bedrock-server-([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)\\.zip"),
+                 zip_url_pat: str = "https:\\/\\/minecraft\\.azureedge\\.net\\/bin-linux\\/" + bds_zip_file_pat,
                  agree_to_meula_and_pp: bool = False) -> None:
         """ McbdscDownloader インスタンスの初期化メソッド。
 
@@ -277,41 +284,59 @@ class FailureAgreeMeulaAndPpError(Exception):
 
 class McbdscDockerManager(object):
     """ Bedrock Server のコンテナとコンテナイメージの作成・管理を行うクラス。
+
+    このクラスが持つ責任範囲は、ローカルに保存されている BDS ファイルを用いて Docker Image を作成し、
+    McbdsDockerContainer インスタンスを管理・制御することです。
     """
 
     def __init__(self,
+                 containers_param: List[dict],
+                 pymcbdsc_root_dir: str = pymcbdsc_root_dir(),
                  docker_client: DockerClient = None,
-                 downloader: McbdscDownloader = McbdscDownloader(),
                  dockerfile: str = "Dockerfile",
+                 bds_zip_dir: str = "downloads",
                  repository: str = "bedrock") -> None:
         """[summary]
 
         Args:
+            containers_param (List[dict]): 管理する全コンテナのパラメータ.
+            pymcbdsc_root_dir (str, optional): pymcbdsc が利用するディレクトリ(フォルダ).
+                                               Defaults to pymcbdsc_root_dir().
             docker_client (DockerClient, optional): Docker ホストに接続する DockerClient インスタンス.
                                                     None の場合は `docker.from_env()` の戻り値を利用する. Defaults to None.
-            downloader (McbdscDownloader, optional): [description]. Defaults to McbdscDownloader().
-            dockerfile (str, optional): [description]. Defaults to "Dockerfile".
+            dockerfile (str, optional): Dockerfile のファイル名. pymcbdsc_root_dir の配下にあるこのファイルを読み込む.
+                                        Defaults to "Dockerfile".
+            bds_zip_dir (str, optional): BDS Zip ファイルが保存されているディレクトリの名前.
+                                         pymcbdsc_root_dir の配下にあるこの名前のディレクトリ内の BDS Zip ファイルを利用する。
+                                         Defaults to "downloads".
             repository (str, optional): [description]. Defaults to "bedrock".
         """
+        self._containers_param = containers_param
+        self._root_dir = pymcbdsc_root_dir
         # 引数のデフォルト値を下記のようにすると、 unittest で import した際に docker.from_env() がコールされてしまい
         # import することも patch することもできない。
         # docker_client: DockerClient = docker.from_env()
         # このため、デフォルト値を None としておき、 None の場合に docker.from_env() をコールする。
         self._docker_client = docker.from_env() if docker_client is None else docker_client
-        self._dockerfile = os.path.join(downloader.root_dir(), dockerfile)
-        self._downloader = downloader
+        self._dockerfile = os.path.join(self._root_dir, dockerfile)
+        self._bds_zip_dir = bds_zip_dir
         self._repository = repository
 
-    def download(self, agree_to_meula_and_pp: bool) -> None:
-        """ Bedrock Server の Zip ファイルをダウンロードするメソッド。
+    def factory_containers(self) -> list:
+        """ McbdscDockerContainer インスタンスを初期化しリストで戻すメソッド。
 
-        This method downloads the Bedrock Server zip file.
-
-        Args:
-            agree_to_meula_and_pp (bool): MEULA 及び Privacy Policy に同意するか否か.
+        Returns:
+            list: McbdscDockerContainer インスタンスのリスト。
         """
-        downloader = self._downloader
-        downloader.download_latest_version_zip_file_if_needed(agree_to_meula_and_pp=agree_to_meula_and_pp)
+        if not hasattr(self, "_containers"):
+            containers_param: list = self._containers_param
+            containers = []
+            docker_client = self._docker_client
+            for container_param in containers_param:
+                container = McbdscDockerContainer(docker_client=docker_client, **container_param)
+                containers.append(container)
+            self._containers = containers
+        return self._containers
 
     def build_image(self, version: str = None, extra_buildargs: dict = None, **extra_build_opt):
         """ Minecraft Bedrock Server の Docker Image を Build するメソッド。
@@ -325,18 +350,17 @@ class McbdscDockerManager(object):
         Returns:
             [type]: Build した Docker Image.
         """
-        dl = self._downloader
         dc_images = self._docker_client.images
-        path = dl.root_dir()
+        root_dir = self._root_dir
         dockerfile = self._dockerfile
         if version is None:
-            version = dl.latest_version()
+            version = self.get_bds_latest_version_from_local_file()
         buildargs = {"BEDROCK_SERVER_VER": version,
-                     "BEDROCK_SERVER_DIR": dl.download_dir(relative=True)}
+                     "BEDROCK_SERVER_DIR": self._bds_zip_dir}
         if extra_buildargs is not None:
             buildargs.update(extra_buildargs)
         tag = "{repository}:{version}".format(repository=self._repository, version=version)
-        return dc_images.build(path=path, dockerfile=dockerfile, buildargs=buildargs, tag=tag, **extra_build_opt)
+        return dc_images.build(path=root_dir, dockerfile=dockerfile, buildargs=buildargs, tag=tag, **extra_build_opt)
 
     def get_image(self, version: str = None):
         """ Minecraft Bedrock Server の、指定されたバージョンの Docker Image を戻すメソッド。
@@ -349,12 +373,85 @@ class McbdscDockerManager(object):
         Returns:
             [type]: 指定されたバージョンの Docker Image.
         """
-        dl = self._downloader
         dc_images = self._docker_client.images
         if version is None:
-            version = dl.latest_version()
+            version = self.get_bds_latest_version_from_local_file()
         tag = "{repository}:{version}".format(repository=self._repository, version=version)
         return dc_images.get(name=tag)
+
+    def get_bds_versions_from_local_file(self, sort=True, reverse=False) -> List[str]:
+        """ ローカルに保存されている BDS Zip ファイルからバージョンの一覧を戻すメソッド。
+
+        sort パラメータを指定しなかった場合は、このメソッドが戻すリストの順番については保証されず、同じ順序であるとも限らない。
+
+        Args:
+            sort (bool, optional): バージョン番号をソートします. reverse が False の場合は昇順でソートします. Defaults to True.
+            reverse (bool, optional): sort が True の場合に、降順でソートします. Defaults to False.
+
+        Returns:
+            List[str]: ローカルに保存されている BDS Zip ファイルから取得したバージョンのリスト.
+        """
+        root_dir = self._root_dir
+        bds_zip_dir = os.path.join(root_dir, self._bds_zip_dir)
+        dir_contents = listdir(bds_zip_dir)
+        files = [c for c in dir_contents if os.path.isfile(os.path.join(bds_zip_dir, c))]
+        if not files:
+            return []
+        bds_zip_file_re = re.compile(bds_zip_file_pat)
+        versions = []
+        for file in files:
+            m = bds_zip_file_re.fullmatch(file)
+            if m:
+                versions.append(m.group(1))
+        if sort:
+            self.sort_bds_versions(versions, reverse)
+        return versions
+
+    @classmethod
+    def sort_bds_versions(cls, versions: List[str], reverse: bool = False) -> None:
+        """
+
+        Args:
+            versions (list): [description]
+
+        Examples:
+
+            >>> from pymcbdsc import McbdscDockerManager
+            >>>
+            >>> versions = [
+            >>>     '1.2.3.4',
+            >>>     '1.1.1.1',
+            >>>     '4.3.2.1',
+            >>>     '1.10.3.4',
+            >>>     '1.10.3.05'
+            >>> ]
+            >>> McbdscDockerManager.sort_bds_versions(versions=versions)
+            >>> versions
+            [
+                '1.1.1.1',
+                '1.2.3.4',
+                '1.10.3.4',
+                '1.10.3.05',
+                '4.3.2.1'
+            ]
+
+        See Also:
+            https://stackoverflow.com/questions/2574080/sorting-a-list-of-dot-separated-numbers-like-software-versions/2574090
+        """
+        versions.sort(key=lambda s: list(map(int, s.split('.'))), reverse=reverse)
+
+    def get_bds_latest_version_from_local_file(self) -> str:
+        """ ローカルに保存されている BDS Zip ファイルの中で最も新しいバージョンを戻すメソッド。
+
+        Returns:
+            str: ローカルに保存されている BDS Zip ファイルの中で最も新しいバージョン.
+            None: ローカルに保存されている BDS Zip ファイルが存在しない場合は None.
+        """
+        versions = self.get_bds_versions_from_local_file()
+        if versions:
+            return self.get_bds_versions_from_local_file()[-1]
+        else:
+            return None
 
 
 class McbdscDockerContainer(object):
