028524e45ed3f2bd1b5c2f47ffaef726d0a769d5
Kodai Tooi <26849015+ktooi@users.noreply.github.com>
2021-01-19 21:48:10 +0900

    Improved codes.

--- manage.py
+++ manage.py
@@ -2,27 +2,45 @@
 # -*- coding: utf-8 -*-
 import os
 import unittest
-from argparse import ArgumentParser
+from logging import basicConfig, getLogger, INFO
+from argparse import ArgumentParser, Namespace
 
 
-def test(args):
+# これはメインのファイルにのみ書く
+basicConfig(level=INFO)
+
+# これはすべてのファイルに書く
+logger = getLogger(__name__)
+
+
+def test(args: Namespace) -> bool:
     runner = unittest.TextTestRunner()
     test_suite = unittest.defaultTestLoader.discover(start_dir="tests", top_level_dir=os.getcwd())
     test_result = runner.run(test_suite)
     return test_result.wasSuccessful()
 
 
-def parse_args():
-    parser = ArgumentParser()
-    subparser = parser.add_subparsers()
+def vercheck(args: Namespace) -> bool:
+    version = os.environ[args.environment] if args.environment else args.version
+    from pymcbdsc import __version__ as pymcbdsc_version
+    act_version = "v{version}".format(version=pymcbdsc_version)
+    logger.info("Pymcbdsc version: {act_version}, Provided version: {exp_version}"
+                .format(act_version=act_version, exp_version=version))
+    return act_version == version
 
-    parser_test = subparser.add_parser('test')
-    parser_test.set_defaults(func=test)
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+    subparsers.required = True
+
+    subcmd_test = subparsers.add_parser('test')
+    subcmd_test.set_defaults(func=test)
 
     return parser.parse_args()
 
 
-def main():
+def main() -> bool:
     args = parse_args()
     return args.func(args)
 
